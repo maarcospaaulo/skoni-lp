0633fe9fd22b5ed3b21b348d1398e1df3be63465
maarcospaaulo <marcoseugenio.dev@gmail.com>
2025-10-30 14:54:15 -0300

    add facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -204,6 +204,7 @@ export default function RootLayout({
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-KV1XGX8P0P"
         />
+        {/* Google Analytics */}
         <Script id="google-analytics">
           {`
             window.dataLayer = window.dataLayer || [];
@@ -213,6 +214,31 @@ export default function RootLayout({
             gtag('config', 'G-KV1XGX8P0P');
           `}
         </Script>
+        {/* Facebook Pixel */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window,document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            
+            fbq('init', '709113565553815'); 
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        {/* Facebook Pixel - NoScript fallback */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=709113565553815&ev=PageView&noscript=1"
+          />
+        </noscript>
       </body>
     </html>
   );
